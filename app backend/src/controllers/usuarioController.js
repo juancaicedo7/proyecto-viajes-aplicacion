b9cc66ce08782067daf87e43736e8cfc3c59f04b
juancaicedo7 <juancaicedo252@gmail.com>
2023-08-05 14:43:50 -0500

    creacion metodo listar todos los usuarios

--- app backend/src/controllers/usuarioController.js
+++ app backend/src/controllers/usuarioController.js
@@ -1,3 +1,11 @@
 import { response } from "../helpers/Response.js";
 
 const usuarioCrtl = {};
+
+usuarioCrtl.listarUsuarios = (req,res) => {
+    try {
+        
+    } catch (error) {
+        response(res, 500, false, "", error.message);
+    }
+}
