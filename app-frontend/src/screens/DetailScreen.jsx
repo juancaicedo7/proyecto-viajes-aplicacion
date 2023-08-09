1f53eaae016db61a884a7ad6673b2c006bfe3626
juancaicedo7 <juancaicedo252@gmail.com>
2023-08-08 19:53:25 -0500

    correcion del navigator y importacion del button de react native

--- app-frontend/src/screens/DetailScreen.jsx
+++ app-frontend/src/screens/DetailScreen.jsx
@@ -1,12 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View,Button } from "react-native";
+import React from "react";
+import { useNavigation } from "@react-navigation/core";
 
 export default function DetailScreen() {
+  const navigation = useNavigation();
+
   return (
     <View>
       <Text>DetailScreen</Text>
+      <Button
+        title="ir a homeScreen"
+        onPress={() => navigation.navigate("HomeScreen")}
+      />
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- app-frontend/src/screens/HomeScreen.jsx
+++ app-frontend/src/screens/HomeScreen.jsx
@@ -1,12 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Button } from "react-native";
+import { useNavigation } from "@react-navigation/core";
 
 export default function HomeScreen() {
+  const navigation = useNavigation();
+
   return (
     <View>
       <Text>HomeScreen</Text>
+      <Button
+        title="ir a detalles"
+        onPress={() => navigation.navigate("DetailScreen")}
+      />
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
